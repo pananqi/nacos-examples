720e26278b0137dd12d6bf8d7d35d9cbddde1ef3
pananqi <541606391@qq.com>
2020-11-14 11:58:46 +0800

    测试修改

--- nacos-spring-cloud-example/nacos-spring-cloud-config-multi-data-ids-example/src/main/java/com/alibaba/nacos/example/spring/cloud/UserApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-multi-data-ids-example/src/main/java/com/alibaba/nacos/example/spring/cloud/UserApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
- * @author xiaojing
+ * @author xiaojing1
  */
 @SpringBootApplication
 public class UserApplication {
